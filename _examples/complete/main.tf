4f76be48b954979e1c752c02e6029ac058d89696
mahesh yadav <devopsmahesh.it@gmail.com>
2023-07-13 17:58:16 +0530

    fix: add digitalocean_record resource

--- _examples/complete/main.tf
+++ _examples/complete/main.tf
@@ -4,7 +4,20 @@ provider "digitalocean" {}
 ## Domain module call
 ##------------------------------------------------
 module "domain" {
-  source     = "./../../"
-  name       = "test.example.com"
-  ip_address = ""
+  source = "./../../"
+  name   = "demo.hello.cam"
+
+  records = {
+    record1 = {
+      type  = "A"
+      name  = "test"
+      value = "192.168.0.12"
+    },
+    record2 = {
+      type  = "A"
+      name  = "demo2"
+      value = "192.168.0.13"
+    },
+  }
+
 }
--- variables.tf
+++ variables.tf
@@ -11,3 +11,27 @@ variable "ip_address" {
   default     = null
   description = "The IP address of the domain. If specified, this IP is used to created an initial A record for the domain."
 }
+variable "records" {
+  type        = map(any)
+  default     = {}
+  description = "Provides a DigitalOcean DNS record resource."
+}
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Whether to create the resources. Set to `false` to prevent the module from creating any resources."
+}
+
+//variable "records" {
+//  type = map(object({
+//    name      = string
+//    type        = string
+//    value    = list(string)
+////    description    = string
+////    operation_name = string
+////    actionGroupID  = string
+////    category       = string
+//  }))
+//
+//  default = {}
+//}
